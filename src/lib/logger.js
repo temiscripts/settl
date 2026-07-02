d285f177953f2235b709622af37441fe58d27f50
temiscripts <mokhefue@gmail.com>
2026-07-02 13:02:40 +0100

    chore: add pino structured logger

---
+++ src/lib/logger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pino = require('pino');
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: process.env.NODE_ENV !== 'production'
+    ? { target: 'pino-pretty', options: { colorize: true, ignore: 'pid,hostname' } }
+    : undefined,
+});
+
+module.exports = logger;
